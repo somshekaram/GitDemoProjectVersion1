ce451e0dfaa9e265150b66e66dec25a4b6ebf555
somashekara <somshekaram@gmail.com>
2019-12-20 16:06:30 +0530

    Second Commit

--- src/programs/ArrayListTest.java
+++ src/programs/ArrayListTest.java
@@ -6,27 +6,28 @@ public class ArrayListTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//ArrayList with no size defined
-		 
-        ArrayList<Integer> list = new ArrayList<>();
- 
-        //Adding elements to ArrayList
- 
-        list.add(10);
- 
-        list.add(20);
- 
-        list.add(30);
- 
-        list.add(40);
- 
-        System.out.println(list.size());     //Output : 4
- 
-        //Removing an element at index 0
- 
-        list.remove(0);
- 
-        System.out.println(list.size());    //Output : 3
+		// ArrayList with no size defined
+
+		ArrayList<Integer> list = new ArrayList<>();
+
+		// Adding elements to ArrayList
+
+		list.add(10);
+
+		list.add(20);
+
+		list.add(30);
+
+		list.add(40);
+		list.add(200);
+
+		System.out.println(list.size()); // Output : 4
+
+		// Removing an element at index 0
+
+		list.remove(0);
+
+		System.out.println(list.size()); // Output : 3
 
 	}
 
